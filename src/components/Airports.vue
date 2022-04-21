290473946e6828203bfba735bba19a4b2d3ae202
Daniel Vaswani <vaswanidaniel@gmail.com>
2022-04-21 21:07:30 +0200

    Complete dropdown styling and functionality

--- src/components/Airports.vue
+++ src/components/Airports.vue
@@ -1,40 +1,72 @@
 <template>
     <section>
-        <h1>Airport</h1>
-        <div v-if="!state.showDropdown" class="current-airport">
-            <h4>John F Kennedy Airport, New York, USA</h4>
-            <h5>JFK</h5>
-            <button @click="true" id="airport-dropdown-icon">
+        <div class="airport-row">
+            <h1>Airport</h1>
+            <div>
+                <button v-if="state.showDropdown" @click="toggleDropdown" id="cancel">Cancel</button>
+            </div>
+        </div>
+        <div v-if="!state.showDropdown" class="airport current-airport">
+            <h4>{{ currentAirport.name }}, {{ currentAirport.city }}, {{ currentAirport.country }}</h4>
+            <h5>{{ currentAirport.iata }}</h5>
+            <button @click="toggleDropdown" id="airport-dropdown-icon">
                 <img src="@/assets/chevron-down-solid.svg" />
             </button>
+        </div>
+        <div v-else class="airport" v-for="a in airports" v-bind:key="a.id">
+            <h4 @click="handleAirportClick(a)">
+                {{ a.name }}
+            </h4>
         </div>
     </section>
 </template>
 
-<script>
-import { reactive } from "vue";
+<script setup>
+import { computed, reactive } from "vue";
+import airportsJson from "@/assets/airports.json";
 
-export default {
-    setup() {
-        const state = reactive({ showDropdown: false });
-        return {
-            state
-        }
-    },
-    components: {
-        // ChevronSVG
-    }
+
+const airports = reactive([...airportsJson])
+const state = reactive({ showDropdown: false, currentId: "3797" })
+
+const currentAirport = computed(() => {
+    return airports.filter(a => a.id === state.currentId)[0];
+})
+
+const toggleDropdown = () => {
+    state.showDropdown = !state.showDropdown
 }
+
+const handleAirportClick = (airport) => {
+    state.currentId = airport.id;
+    toggleDropdown()
+}
+
+console.log(airports)
 
 </script>
 
-<style>
-.current-airport {
+<style scoped>
+.airport-row::before {
+    content: '';
+}
+
+.airport-row {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+.airport {
     display: flex;
     justify-content: space-around;
     align-items: center;
     gap: 2rem;
     border: 1px solid lightgrey;
+}
+
+.airport h4 {
+    cursor: pointer;
 }
 
 h5 {
